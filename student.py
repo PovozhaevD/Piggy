ada1c4555408e0d2a6a8d3659ad6b0c7521ba086
Dominik Povozhaev <91563329+PovozhaevD@users.noreply.github.com>
2021-11-09 19:47:34 +0000

    test

--- student.py
+++ student.py
@@ -63,7 +63,7 @@ class Piggy(PiggyParent):
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
-        self.safe_to_dance()
+        """self.safe_to_dance()"""
         # lower-ordered example...
         self.right(primary=50, counter=50)
         self.left(primary=50, counter=50)
