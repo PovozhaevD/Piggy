f3348dcea8e5e47cb9c16c6e6fda2d5549d0a0bd
Dominik Povozhaev <91563329+PovozhaevD@users.noreply.github.com>
2021-12-14 19:26:19 +0000

    fix

--- student.py
+++ student.py
@@ -192,15 +192,15 @@ class Piggy(PiggyParent):
       self.servo(self.MIDPOINT)
       if "R" in direction:
         self.right(primary=100, counter=50) 
-        time.sleep(1)
+        time.sleep(2)
         self.stop()
         self.left(primary=100, counter=50) 
       elif "L" in direction:
         self.left(primary=100, counter=50)
-        time.sleep(1)
+        time.sleep(2)
         self.stop()
         self.right(primary=100, counter=50) 
-        time.sleep(1)
+        time.sleep(2)
         self.stop()
 
 
@@ -216,13 +216,13 @@ class Piggy(PiggyParent):
           self.servo(2000)
           self.stop()
           time.sleep(.2)
-          if (self.read_distance() < 200):
+          if (self.read_distance() < 300):
             self.m_swerve("L")
             self.servo(1800)
-          if (self.read_distance() < 200):
+          if (self.read_distance() < 300):
             self.m_swerve("R")
             self.servo(1400)
-          if (self.read_distance() < 200):
+          if (self.read_distance() < 300):
             self.right(primary=100, counter=-100)
             time.sleep(0.3)
             self.stop()
